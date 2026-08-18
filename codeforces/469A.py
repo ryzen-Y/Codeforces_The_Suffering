30ab5807f9a0a241e197011f207dc0dc8d73daae
ryzen <mirzaaliuntauhid@gmail.com>
2026-08-19 02:35:49 +0600

    solution

--- codeforces/469A.py
+++ codeforces/469A.py
@@ -1,14 +1,16 @@
 n = int(input())
 
-p = int(input())
 x = list(map(int, input().split()))
+p = x[0]
+x = x[1:]
 
-q = int(input())
 y = list(map(int, input().split()))
+q = y[0]
+y = y[1:]
 
-levels = set(x + y)
+lst = x + y
 
-if all(i in levels for i in range(1, n + 1)):
+if all(i in lst for i in range(1, n + 1)):
     print("I become the guy.")
 else:
     print("Oh, my keyboard!")
